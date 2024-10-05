d0bbc284cb030311dfa81663c35f1de184bf6669
Jonas Marty <jonas.marty@konplan.com>
2024-10-05 20:23:28 +0200

    seed2

---
+++ GamblingMoneyTracker/Migrations/20241005182136_Seed2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamblingMoneyTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE * FROM Players");
+            migrationBuilder.InsertData("Players", ["Name", "PlayMoney"], ["Migros", 0]);
+            migrationBuilder.InsertData("Players", ["Name", "PlayMoney"], ["Nestle", 0]);
+            migrationBuilder.InsertData("Players", ["Name", "PlayMoney"], ["Kambli", 0]);
+            migrationBuilder.InsertData("Players", ["Name", "PlayMoney"], ["Post", 0]);
+            migrationBuilder.InsertData("Players", ["Name", "PlayMoney"], ["Landi", 0]);
+            migrationBuilder.InsertData("Players", ["Name", "PlayMoney"], ["SBB", 0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
